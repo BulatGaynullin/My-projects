ad241a31364427d7c2113a5ad81a152bf419c319
BulatGaynullin <vafin9651@gmail.com>
2024-11-19 19:17:01 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = tuple_ = (5, 6, "r" , "t" )
+print(immutable_var)
+#tuple_[0] = 200 #Кортежи в Python неизменяемы
+
+mutable_list = tuple_1 = [9, 2, "rok", "cola"]
+print(mutable_list)
+tuple_1[2] = 7
+print(mutable_list)
+
+
